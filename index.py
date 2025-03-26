1ffcdcb8e1331bb6eb52d51a515356dd23d3f86a
BaldeKadiatou <kadidiaabdul.balde@gmail.com>
2025-03-26 09:52:44 +0300

    lists in python

---
+++ index.py
@@ -0,0 +1,23 @@
+#creating an empty list
+my_list=[] 
+#printing the empty list
+print(my_list) 
+#adding elements to the list using append method
+print(my_list)
+my_list.append(10) 
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+#inserting an element at the second position of the list using insert method
+my_list.insert(1,15)
+print(my_list)
+#extending the list with another list using extend method
+my_list.extend([50,60,70])
+print(my_list)
+#removing an element from the list using remove method
+my_list.remove(70)
+print(my_list)
+#sortint the list in ascending order using sort method
+my_list.sort()
+print(my_list)
